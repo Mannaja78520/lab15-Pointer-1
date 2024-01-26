0d5ab29290f252fb9a13ecdc9639ca3142b60023
mannaja <manman7852078520@gmail.com>
2024-01-26 16:34:18 +0700

    lab 15_4 completed

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,8 +1,25 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
 
-	// cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << endl;
-	// *y = "W";
-	// cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << endl;
-	// *x = 6;
-	// cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << endl;
-	// *z = 7;
-	// cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << endl;
+void shuffle(int &,int &,int &,int &);
+
+int main(){
+	int a = 50, b = 100, c = 500, d = 1000;
+	
+	srand(time(0));	
+	
+	for(int i = 0;i < 10;i++){
+	    shuffle(a,b,c,d);
+	    cout << a << " " << b << " " << c << " " << d << "\n";
+	}
+	
+	return 0;
+}
+
+void shuffle(int &a, int &b, int &c, int &d){
+	int Rand = rand() % 4;
+	int A[4] = {a, b, c, d}; 
+	swap(A[Rand], A[Rand-1]);
+}
